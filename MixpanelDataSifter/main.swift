4970ad82971ccf079c2a4a79d23cd82ed3f792cf
Worth Baker <wbaker@greenmtd.com>
2016-12-13 09:40:11 -0700

    Initial Commit

---
+++ MixpanelDataSifter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MixpanelDataSifter
+//
+//  Created by David Baker on 12/13/16.
+//  Copyright © 2016 redpointcollaborative.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
